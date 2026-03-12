a07ad5e0b3533905b872c257a39f118b3cc08742
S.Khalezov <energet666@gmail.com>
2026-03-12 20:05:20 +0500

    fix(frontend): make fonts available offline

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,10 @@
 import { mount } from 'svelte'
 import './app.css'
+import '@fontsource/outfit/300.css'
+import '@fontsource/outfit/400.css'
+import '@fontsource/outfit/500.css'
+import '@fontsource/outfit/600.css'
+import '@fontsource/outfit/700.css'
 import App from './App.svelte'
 
 const app = mount(App, {
